6379fed8d625aeb12a50d2c21982b4f2dd949cb3
Emilio Monroy <emilio.monroy@kantar.com>
2022-03-21 13:53:42 +0100

    Actions and stories added

--- actions/actions.py
+++ actions/actions.py
@@ -9,49 +9,129 @@ import random
  
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import FollowupAction
+
+import os
+import json
  
 # computer_choice & determine_winner functions refactored from
 # https://github.com/thedanelias/rock-paper-scissors-python/blob/master/rockpaperscissors.py, MIT liscence
  
-class ActionPlayRPS(Action):
+
+def get_schedules_(trip_id, json_trip_data):
+    schedule_ids = None
+    for trip_schedule in  json_trip_data['trips_schedules']:
+        if trip_schedule['trip_id'] == trip_id:
+            schedule_ids = trip_schedule['schedule_id']
+            break
+    
+    schedules_list = []
+    for schedules in json_trip_data['schedules']:
+        if schedules['schedule_id'] in schedule_ids:
+            schedules_list.append(schedules['local_hour'])
+
+    return(schedules_list, schedule_ids)
+
+
+class DisplayTrips(Action):
    
     def name(self) -> Text:
-        return "action_play_rps"
- 
-    def computer_choice(self):
-        generatednum = random.randint(1,3)
-        if generatednum == 1:
-            computerchoice = "rock"
-        elif generatednum == 2:
-            computerchoice = "paper"
-        elif generatednum == 3:
-            computerchoice = "scissors"
-       
-        return(computerchoice)
+        return "action_display_trips"
  
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
  
-        # play rock paper scissors
-        user_choice = tracker.get_slot("choice")
-        dispatcher.utter_message(text=f"You chose {user_choice}")
-        comp_choice = self.computer_choice()
-        dispatcher.utter_message(text=f"The computer chose {comp_choice}")
- 
-        if user_choice == "rock" and comp_choice == "scissors":
-            dispatcher.utter_message(text="Congrats, you won!")
-        elif user_choice == "rock" and comp_choice == "paper":
-            dispatcher.utter_message(text="The computer won this round.")
-        elif user_choice == "paper" and comp_choice == "rock":
-            dispatcher.utter_message(text="Congrats, you won!")
-        elif user_choice == "paper" and comp_choice == "scissors":
-            dispatcher.utter_message(text="The computer won this round.")
-        elif user_choice == "scissors" and comp_choice == "paper":
-            dispatcher.utter_message(text="Congrats, you won!")
-        elif user_choice == "scissors" and comp_choice == "rock":
-            dispatcher.utter_message(text="The computer won this round.")
+
+        #dispatcher.utter_message(text=f"Hola mundo")
+        departure_city = tracker.get_slot("departure_city")
+
+        f = open('data/trips.json')
+        json_trip_data = json.load(f)
+
+        list_trips = []
+        for trip in json_trip_data['trips']:
+            if trip['origin'] == departure_city:
+                list_trips.append(trip)
+        
+        if list_trips:
+            dispatcher.utter_message(text=f"Here are the destinations for {departure_city}")
+            for trip in list_trips:
+                trip_schedules,  _= get_schedules_(trip['trip_id'], json_trip_data)
+                string_schedules = ' '.join(trip_schedules)
+                dispatcher.utter_message(text=f"Destination {trip['destination']} - {string_schedules}")
+
+            #return [FollowupAction(name='utter_which_arrival_city')]
+        
         else:
-            dispatcher.utter_message(text="It was a tie!")
- 
+            dispatcher.utter_message(text=f"We weren't able to find a trip from {departure_city}")
+            
         return []
+
+class DisplaySeats(Action):
+   
+    def name(self) -> Text:
+        return "action_display_seats"
+ 
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+ 
+
+        departure_city = tracker.get_slot("departure_city")
+        arrival_city = tracker.get_slot("arrival_city")
+        departure_time = tracker.get_slot("departure_time")
+
+        f = open('data/trips.json')
+        json_trip_data = json.load(f)
+
+        list_trips = []
+        for trip in json_trip_data['trips']:
+            if trip['origin'] == departure_city:
+                list_trips.append(trip)
+        
+        trip_id = None
+        if list_trips:
+            for trip in list_trips:
+                if trip['destination'] == arrival_city:
+                    trip_id = trip['trip_id']
+                    break
+            if trip_id is not None:
+                trip_schedules, schedule_ids = get_schedules_(trip_id, json_trip_data)
+                schedule_id = None
+                for trip_schedule, schdl_id in zip(trip_schedules, schedule_ids):
+                    if departure_time == trip_schedule:
+                        schedule_id = schdl_id
+                        break
+
+                if schedule_id is not None:
+                    current_info = json_trip_data['current_info'] 
+                    price_seats = None
+                    for cnt_info in current_info:
+                        if (cnt_info["trip_id"] == trip_id) & (cnt_info["schedule_id"] == schedule_id):
+                            price_seats = cnt_info["price_seats"]
+                            break
+
+                    if price_seats is not None:
+                        dispatcher.utter_message(text=f"Available seats:")
+                        dispatcher.utter_message(text=f"First class:")
+                        dispatcher.utter_message(text=f"    Available seats {price_seats[0]} - Price €{price_seats[1]}")
+                        dispatcher.utter_message(text=f"Second class:")
+                        dispatcher.utter_message(text=f"    Available seats {price_seats[2]} - Price €{price_seats[2]}")
+                    else:
+                        dispatcher.utter_message(text=f"{departure_city} - {arrival_city} - {departure_time}")
+                        #dispatcher.utter_message(text=f"DEBUG04: We weren't able to find a trip from {departure_city} to {arrival_city} at {departure_time}")
+                else:
+                    dispatcher.utter_message(text=f"{departure_city} - {arrival_city} - {departure_time}")
+                    #dispatcher.utter_message(text=f"DEBUG03: We weren't able to find a trip from {departure_city} to {arrival_city} at {departure_time}")
+
+            else:
+                dispatcher.utter_message(text=f"{departure_city} - {arrival_city} - {departure_time}")
+                #dispatcher.utter_message(text=f"DEBUG02: We weren't able to find a trip to {arrival_city}")
+        
+        else:
+            dispatcher.utter_message(text=f"{departure_city} - {arrival_city} - {departure_time}")
+            #dispatcher.utter_message(text=f"DEBUG01: We weren't able to find a trip from {departure_city}")
+            
+        return []
+ 
